103e7990f14e477da49f5e0ddd11bcb39b2f619d
Paulo Sergio Vieira de Oliveira <paulosergiooliveira113@gmail.com>
2026-09-20 18:11:54 -0300

    feat: add NovoItemScreen for inventory item creation and integrate axios for API calls

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,12 +1,14 @@
 import { Text, View } from 'react-native';
+import NovoItemScreen from './novo-item';
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-slate-900">
-      <Text className="text-2xl font-bold text-white">AGIL</Text>
-      <Text className="text-slate-400 mt-2">
-        Project Running with Native + Expo + NativeWind (tailwind)
-      </Text>
-    </View>
+    // <View className="flex-1 items-center justify-center bg-slate-900">
+    //   <Text className="text-2xl font-bold text-white">AGIL</Text>
+    //   <Text className="text-slate-400 mt-2">
+    //     Project Running with Native + Expo + NativeWind (tailwind)
+    //   </Text>
+    // </View>
+    <NovoItemScreen />
   );
 }
