8aa5b02ad4bf3cf45f5402b35d1687f73ec621e4
Edivar Cruz <edivarfilho16@gmail.com>
2026-09-24 13:22:29 -0300

    feat(inventory): add authentication and authorization guards to inventory controller

--- api/src/inventory/inventory.controller.ts
+++ api/src/inventory/inventory.controller.ts
@@ -1,11 +1,16 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { InventoryService } from './inventory.service.js';
 import { CreateInsumoDto } from './dtos/create-insumo.dto.js';
+import { Roles } from '../authz/roles.decorator.js';
+import { RolesGuard } from '../authz/roles.guard.js';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('inventory')
+@UseGuards(AuthGuard('jwt'), RolesGuard)
 export class InventoryController {
   constructor(private readonly inventoryService: InventoryService) {}
 
+  @Roles('TECNICO')
   @Post('insumos')
   async criarInsumo(@Body() data: CreateInsumoDto) {
     return this.inventoryService.criarInsumo(data);
